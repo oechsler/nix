c62804606935ababff03c7a1308410a0ba128e38
Samuel Oechsler <samuel@oechsler.it>
2026-08-14 14:25:17 +0200

    refactor(desktop): isolate Cinny configuration

--- modules/home-manager/programs/apps.nix
+++ modules/home-manager/programs/apps.nix
@@ -38,7 +38,6 @@
 # - libsecret - Tools for accessing gnome-keyring (used by Chrome, Vesktop, etc.)
 
 {
-  config,
   pkgs,
   features,
   displays,
@@ -84,46 +83,6 @@ let
       enable = hasHDR;
     };
 
-  cinnyUserstyle = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/userstyles/main/styles/cinny/catppuccin.user.less";
-    hash = "sha256-yOXRnkqN1ApnpOyfNRXcJdY9mKf0VrAFcZMAaHiXla8=";
-  };
-  cinnyCatppuccinLib = pkgs.fetchurl {
-    url = "https://userstyles.catppuccin.com/lib/std/v1.less";
-    hash = "sha256-XK9Oqan7Kz81DNyE3+ryl5sPi/OpvV+EkgL7WuLoGfM=";
-  };
-  cinnyStyle = pkgs.runCommand "cinny-catppuccin.css" { } ''
-    cp ${cinnyUserstyle} style.less
-    sed -i \
-      -e '/^\/\* ==UserStyle==/,/^==\/UserStyle== \*\//d' \
-      -e '/^@-moz-document domain("cinny.in") {/d' \
-      -e '$d' \
-      -e 's#https://userstyles.catppuccin.com/lib/lib.less#${cinnyCatppuccinLib}#' \
-      -e '/https:\/\/prismjs.catppuccin.com\/variables.important.css/d' \
-      -e 's/@lightFlavor/${theme.catppuccin.flavor}/g' \
-      -e 's/@darkFlavor/${theme.catppuccin.flavor}/g' \
-      -e 's/@accentColor/${theme.catppuccin.accent}/g' \
-      style.less
-    sed -i '1i @accentColor: ${theme.catppuccin.accent};' style.less
-    ${pkgs.lessc}/bin/lessc style.less $out
-  '';
-  cinnyUnwrapped = pkgs.cinny.overrideAttrs (old: {
-    postInstall = (old.postInstall or "") + ''
-      cp ${cinnyStyle} $out/catppuccin.css
-      ${pkgs.gnused}/bin/sed -i \
-        's#</head>#<link rel="stylesheet" href="./catppuccin.css"></head>#' \
-        $out/index.html
-    '';
-  });
-  cinnyDesktop = pkgs.cinny-desktop.overrideAttrs (old: {
-    postPatch = (old.postPatch or "") + ''
-      ${pkgs.jq}/bin/jq \
-        --arg frontendDist "${cinnyUnwrapped}" \
-        '.build.frontendDist = $frontendDist' \
-        tauri.conf.json | ${pkgs.moreutils}/bin/sponge tauri.conf.json
-    '';
-  });
-
 in
 {
   #===========================
@@ -143,7 +102,6 @@ in
             [
               alsa-scarlett-gui
               mumble
-              cinnyDesktop
               (wrapChromiumApp vesktop "vesktop")
               freecad
               libreoffice
