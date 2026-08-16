ffda07dc7ea6e12fe09f8701941195b3f07c173c
Samuel Oechsler <samuel@oechsler.it>
2026-08-17 00:08:11 +0200

    fix(virtualisation): restore libvirt network on boot

--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -9,12 +9,32 @@
 
 {
   lib,
+  pkgs,
   config,
   ...
 }:
 
 let
   cfg = config.features.virtualisation;
+  defaultNetworkXml = pkgs.writeText "libvirt-default-network.xml" ''
+    <network>
+      <name>default</name>
+      <forward mode='nat'>
+        <nat ipv6='yes'/>
+      </forward>
+      <bridge name='virbr0' stp='on' delay='0'/>
+      <ip address='192.168.122.1' netmask='255.255.255.0'>
+        <dhcp>
+          <range start='192.168.122.2' end='192.168.122.254'/>
+        </dhcp>
+      </ip>
+      <ip family='ipv6' address='fd42:122:122::1' prefix='64'>
+        <dhcp>
+          <range start='fd42:122:122::2' end='fd42:122:122::ffff'/>
+        </dhcp>
+      </ip>
+    </network>
+  '';
 in
 {
   options.features.virtualisation = {
@@ -52,6 +72,28 @@ in
 
         programs.virt-manager.enable = true;
         users.users.${config.user.name}.extraGroups = [ "libvirtd" ];
+
+        systemd.services.libvirt-default-network = {
+          description = "Ensure the libvirt default network is available";
+          wantedBy = [ "multi-user.target" ];
+          wants = [ "network-online.target" ];
+          after = [ "libvirtd.service" "network-online.target" ];
+          requires = [ "libvirtd.service" ];
+          serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+            ExecStart = pkgs.writeShellScript "libvirt-default-network" ''
+              if ! ${pkgs.libvirt}/bin/virsh -c qemu:///system net-info default >/dev/null 2>&1; then
+                ${pkgs.libvirt}/bin/virsh -c qemu:///system net-define ${defaultNetworkXml}
+              fi
+
+              ${pkgs.libvirt}/bin/virsh -c qemu:///system net-autostart default
+              if ! ${pkgs.libvirt}/bin/virsh -c qemu:///system net-info default | ${pkgs.gnugrep}/bin/grep -q 'Active:.*yes'; then
+                ${pkgs.libvirt}/bin/virsh -c qemu:///system net-start default
+              fi
+            '';
+          };
+        };
 
         # virt-manager is launched through a Nix wrapper. Keep its wrapper
         # class for window matching, but use the packaged icon directly.
