f2cdabfb929b27947bdd6676ebd5b0d1f3c8dc18
Samuel Oechsler <samuel@oechsler.it>
2026-08-24 02:06:18 +0200

    fix: finalize installer launch and workspace spacing

--- modules/installer/iso.nix
+++ modules/installer/iso.nix
@@ -45,32 +45,24 @@
   ];
 
   environment.etc = {
-    "nixos-installer/manifest.json".text = builtins.toJSON hostManifest;
+    "nixos-installer/manifest.json".text = builtins.toJSON { hosts = hostManifest; };
     "nixos-installer/install.sh".source = ../../install.sh;
     "nixos-installer/repo".source = ../../.;
-    "xdg/autostart/nixos-installer.desktop".text = ''
+    "nixos-installer/NixOS Installer.desktop".text = ''
       [Desktop Entry]
       Type=Application
       Name=NixOS Installer
       Comment=Install a prebuilt NixOS host
-      Exec=${pkgs.kdePackages.konsole}/bin/konsole -e ${pkgs.sudo}/bin/sudo ${pkgs.bash}/bin/bash /etc/nixos-installer/install.sh --iso
+      Exec=${pkgs.kdePackages.konsole}/bin/konsole -e /run/wrappers/bin/sudo ${pkgs.bash}/bin/bash /etc/nixos-installer/install.sh --iso
       Terminal=false
       Categories=System;
     '';
   };
 
-  systemd.user.services.nixos-installer = {
-    description = "NixOS graphical installer";
-    after = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    wantedBy = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "exec";
-      ExecStart = "${pkgs.kdePackages.konsole}/bin/konsole --hold -e ${pkgs.sudo}/bin/sudo ${pkgs.bash}/bin/bash /etc/nixos-installer/install.sh --iso";
-      Restart = "on-failure";
-      RestartSec = 2;
-    };
-  };
+  systemd.tmpfiles.rules = [
+    "d /home/nixos/Desktop 0755 nixos users - -"
+    "C /home/nixos/Desktop/NixOS Installer.desktop 0755 nixos users - /etc/nixos-installer/NixOS Installer.desktop"
+  ];
 
   isoImage.storeContents = lib.attrValues hostClosures;
 }
