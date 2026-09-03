d155f21a5e39955679602f8e5b4b0d88a53c7a37
Samuel Oechsler <samuel@oechsler.it>
2026-09-04 01:10:32 +0200

    Revert "fix: retry failed FIDO2 unlocks"
    This reverts commit edd413004b717b8fe5f5a1c3d375228e6fd0a954.

--- modules/system/auth/luks.nix
+++ modules/system/auth/luks.nix
@@ -113,16 +113,9 @@ in
         name: _:
         lib.nameValuePair "systemd-cryptsetup@${name}" {
           overrideStrategy = "asDropin";
-          # libfido2 aborts an unanswered touch request after about 30 seconds.
-          # Retry instead of letting cryptsetup.target enter the rescue shell.
-          unitConfig.StartLimitIntervalSec = "0";
           after = [ "fido2-yubikey-wait.service" ];
           wants = [ "fido2-yubikey-wait.service" ];
-          serviceConfig = {
-            Restart = "on-failure";
-            RestartSec = "1s";
-            TimeoutStartSec = "infinity";
-          };
+          serviceConfig.TimeoutStartSec = "infinity";
         }
       ) devices;
     };
