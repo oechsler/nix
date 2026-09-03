0f55e37b55016578f44b5d27d91fc41df281223c
Samuel Oechsler <samuel@oechsler.it>
2026-09-03 21:30:33 +0200

    remove(opencode): drop server and caddy

--- modules/system/impermanence.nix
+++ modules/system/impermanence.nix
@@ -102,14 +102,6 @@ in
       ++ lib.optionals config.features.desktop.enable [
         "/var/lib/sddm" # SDDM state
       ]
-      ++ lib.optionals (config.services.localEndpoints != { }) [
-        {
-          directory = "/var/lib/caddy";
-          user = "caddy";
-          group = "caddy";
-          mode = "0755";
-        }
-      ]
       ++ lib.optionals config.features.auth.ldap.enable [
         "/var/lib/pam-lldap" # Cached LDAP password verifier
       ]
